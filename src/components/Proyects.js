7fea9dfb948f73f3cab451b52c8bbec6cb953382
Jhonatan Marin <gohan44marin@gmail.com>
2023-05-26 01:22:27 -0500

    se adiciona el url al repositorio en proyects

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -11,7 +11,7 @@ export const Proyects = () => {
       <div >
         <h5>Curriculum with React</h5>
         <div className='proyects_text'>
-          <a className='proyects_text--link' href=''><b>Curriculum-React</b></a>
+          <a className='proyects_text--link' href='https://github.com/MARIN4444/curriculum-react'><b>Curriculum-React</b></a>
           <br/>
           <p className='education_text--text'>project where a Single Page Application is made with the basic information of my curriculum
           using 'bootstrap' as framework, <br/>implementing languages ​​for web pages (html, css. javascript) with react (jsx) and applying a bem methodology for css</p>
