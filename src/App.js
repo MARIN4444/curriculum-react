012de54f04f615563dc198ef1741546da00befc6
Jhonatan Marin <gohan44marin@gmail.com>
2023-05-26 01:21:32 -0500

    se adicionan los meta en el app.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,11 @@ import { Skills } from './components/Skills';
 import { SocialMedia } from './components/SocialMedia';
 
 
-
-
-
 function App() {
   return (
     <div className="App">
+      <meta name='author' content='Manuela Velásquez - Jhonatan Marín'></meta>
+      <meta name='keywords' content='curriculum, react, html, css'></meta>
       <div className='Container'>
         <div className='Container_Left'>
           <AboutMe/>
