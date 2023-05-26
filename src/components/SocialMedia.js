dcbf83f94921c950d48b128dbd7f0c982df0130b
Jhonatan Marin <gohan44marin@gmail.com>
2023-05-26 00:11:00 -0500

    se adapta la class de botones a la metodologia bem

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,7 +4,7 @@ import img from '../img/img'
 export const SocialMedia = () => {
   return (
     <div className='SocialMedia'>
-        <ul className='botones'>
+        <ul className='SocialMedia_botones'>
             <li><a href='https://www.facebook.com/profile.php?id=100004806431119' target='_blank'><img src={img.facebook} className='btn_social'></img></a></li>
             <li><a href='https://www.instagram.com/marin_s44/' target='_blank'><img src={img.instagram} className='btn_social'></img></a></li>
             <li><a href='https://www.linkedin.com/in/marin-m-685407137/' target='_blank'><img src={img.linkedin} className='btn_social'></img></a></li>
