294f3cf72b7589624f041a29083855007ec3b0f7
Jhonatan Marin <gohan44marin@gmail.com>
2023-05-26 00:56:58 -0500

    se agrega el contenido del componente 'me'

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import img from '../img/img'
 
 export const Me = () => {
   return (
-    <div>Me</div>
+    <div>
+        <div className='me'>
+            <img src={img.me} className='me_img'></img>
+            <h2 className='me_tittle'>Jhonatan Marin Salazar<br/>System Engineer</h2>
+        </div>
+    </div>
   )
 }
